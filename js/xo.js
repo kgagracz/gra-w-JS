373b10004107e7fa538b831e2b0042c7fcfdaf28
Krystian Gagracz <krystian.gagracz@gmail.com>
2020-01-28 16:55:52 +0100

    game eneded

--- js/xo.js
+++ js/xo.js
@@ -1,33 +1,54 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 
-    let gameFields = document.querySelectorAll('table td button')
+    const gameFields = document.querySelectorAll('table td button')
     let didPlayerClicked = false; 
+    const xClass = 'X';
+    const circleClass = 'O';
+    const winningCombinations = [
+        [0, 1 , 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8]
+    ];
+    const winningP = document.getElementById('winning-message');
+    const winningMessage = ' zwycięża!' 
+
     for(let i=0; i<gameFields.length; i++) {
         
-        //change a background of field after click 
-        gameFields[i].addEventListener('click', () => {
+        gameFields[i].addEventListener('click',  () => {
+
+            const currentClass = didPlayerClicked ? circleClass : xClass;
+            console.log(currentClass);
+
+            gameFields[i].classList.add(currentClass);
 
             if (didPlayerClicked === false) {
                 gameFields[i].style.backgroundImage = 'url(img/x.png)';
-                console.log('zmieniono na krzyżyk');
                 didPlayerClicked = true;
             }
             else if (didPlayerClicked === true) {
                 gameFields[i].style.backgroundImage = 'url(img/o.png)';
-                console.log('zmieniono na kółko');
                 didPlayerClicked = false;
             }
             
-        })
+            if (checkWin(currentClass)) {
+                winningP.innerText = currentClass + winningMessage;
+                winningP.style.backgroundColor = 'lightgreen';
+            }
+        }, {once: true})
+
         
     }
-
-
-
-
-
-
-
-
-
+    
+    checkWin = (currentClass) => {
+        return winningCombinations.some(combination => {
+            return combination.every(index => {
+                return gameFields[index].classList.contains(currentClass)
+            })
+        })
+    }
 });
